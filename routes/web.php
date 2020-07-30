e40448d04cae51f48b87dbd60971937a107e42bc
Eugene <goncharovei@gmail.com>
2020-07-30 09:45:25 +0300

    added project

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth', 'roles'], 'roles' => 'admin'], function () {
+	Route::get('/', 'AdminController@index')->name('admin_panel');
+	Route::resource('roles', 'RolesController');
+	Route::resource('permissions', 'PermissionsController');
+	Route::resource('users', 'UsersController');
+	Route::resource('pages', 'PagesController');
+	Route::resource('activitylogs', 'ActivityLogsController')->only([
+		'index', 'show', 'destroy'
+	]);
+	Route::resource('settings', 'SettingsController');
+	Route::get('generator', ['uses' => '\Appzcoder\LaravelAdmin\Controllers\ProcessController@getGenerator']);
+	Route::post('generator', ['uses' => '\Appzcoder\LaravelAdmin\Controllers\ProcessController@postGenerator']);
+});
+
+Auth::routes();
+
